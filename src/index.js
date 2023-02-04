75809826c45c4d163f977bf622c9f7aed34626f1
Andrea <andrearivas4cinfor@gmail.com>
2023-02-04 16:41:48 +0100

    get first geolocalization data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import _ from 'lodash';
 import './style.css';
-
- function component() {
-   const element = document.createElement('div');
-
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-   return element;
+ const key =  '5157c507d51ade4731309623a34583e2';
+ const urlWeather =  'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+ const urlGeoloc = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`
+  
+ const city = 'Madrid';
+ const getData = async () => {
+  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`);
+  const data = await response.json();
+  console.log(data)
  }
-
- document.body.appendChild(component());
+ getData();
